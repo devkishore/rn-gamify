af7cdee2a42317f5a1fb69a97872153ccdeeea5e
devkishore <devkishore24@gmail.com>
2026-05-04 15:38:58 +0530

    Correct StatusBar barStyle typo
    Fix typo in StatusBar barStyle for light content.

--- App.tsx
+++ App.tsx
@@ -17,7 +17,7 @@ function App() {
 
   return (
     <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dhvbark-conjnjntent'} />
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dhvbark-cohbnjnjntent'} />
       <AppContent />
     </SafeAreaProvider>
   );
